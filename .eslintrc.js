686f0b9b454c00a6ea0c2c573c24285f1b7f77e9
Ryan Northey <ryan@synca.io>
2020-02-21 18:38:25 +0000

    Add javascript setup

---
+++ .eslintrc.js
@@ -0,0 +1,59 @@
+module.exports = {
+    "extends": [
+	"eslint:recommended",
+	"plugin:react/recommended"
+    ],
+    settings: {
+	"propWrapperFunctions": ["exact"]},
+    env: {
+	browser: true,
+	"jest/globals": true
+    },
+    parser: "babel-eslint",
+    parserOptions: {
+	ecmaVersion: 6,
+	ecmaFeatures: {
+	    jsx: true,
+	},
+	sourceType: "module",
+    },
+    globals: {
+	gettext: false,
+	ngettext: false,
+	interpolate: false,
+	l: false,
+	expect: false,
+	test: false,
+	browser: false,
+	jest: false,
+	Promise: false,
+	require: false,
+	shortcut: false,
+	sorttable: false,
+	module: false,
+	global: false
+    },
+    plugins: [
+	"react",
+	"babel",
+	"jest"
+    ],
+    rules: {
+	"react/prefer-es6-class": 1,
+	"react/prefer-stateless-function": 1,
+	"react/prefer-stateless-function": [1, {"ignorePureComponents": true}],
+	"react/prop-types": 1,
+	"react/jsx-key": 1,
+	"react/jsx-uses-react": 1,
+	"react/jsx-uses-vars": 1,
+	"babel/new-cap": 1,
+	"babel/camelcase": 1,
+	"babel/no-invalid-this": 1,
+	"babel/object-curly-spacing": 1,
+	"babel/quotes": 1,
+	"babel/semi": 1,
+	"babel/no-unused-expressions": 1,
+	"babel/valid-typeof": 1,
+	"no-unused-vars": [1, { "ignoreRestSiblings": true }]
+    },
+};
---
+++ babel.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    "presets": [
+	"@babel/preset-react", "@babel/preset-env"
+    ],
+    "plugins": [
+	"@babel/plugin-proposal-class-properties",
+	"@babel/plugin-proposal-object-rest-spread",
+	"rewire",
+	"transform-import-css"
+    ]
+};
